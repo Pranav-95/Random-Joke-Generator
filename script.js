33abe135a8c3f6dc624dd69b52943c520721a20a
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-23 13:56:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const jokeEl = document.getElementById('joke');          // get the joke from id
 const get_joke = document.getElementById('get_joke');    // get the get_joke from id in button from HTML file in line 17.
 
 get_joke.addEventListener('click', generateJoke);
-generateJoke();         // call the function 
+generateJoke();                                          // call the function 
 
 async function generateJoke(){
     // call the icanhaz API
-    const jokeRes = await fetch('https://icanhazdadjoke.com/', {     // the fetch function
+    const jokeRes = await fetch('https://icanhazdadjoke.com/', {     // when button is clicked we make another request to the API to get another joke and save it again, basically change the innerHTML.
         headers: {
             'Accept': 'application/json'       // this will tell that we need a JSON response
 
@@ -17,6 +17,6 @@ async function generateJoke(){
     const joke = await jokeRes.json();     // save the joke 
 
     // set the new joke
-    jokeEl.innerHTML = joke.joke;    // 
+    jokeEl.innerHTML = joke.joke;    // store the response joke inside the div
 }
 
