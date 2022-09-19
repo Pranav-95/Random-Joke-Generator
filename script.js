057d8ebb52b7babb617ac6803f6e492b194bdfc1
Chirantan Banik <banikchirantan16@gmail.com>
2022-09-19 10:54:32 +0530

    Changes made

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const jokeEl = document.getElementById('joke');        
-const get_joke = document.getElementById('get_joke');    
+const jokeEl = document.getElementById('joke');          // div id = joke        
+const get_joke = document.getElementById('get_joke');    // button id = get_joke 
 
 get_joke.addEventListener('click', generateJoke);
 generateJoke();                                          
 
-async function generateJoke(){
+async function generateJoke(){  // async makes a function return a Promise
     // call the icanhaz API
-    const jokeRes = await fetch('https://icanhazdadjoke.com/', {    
+    const jokeRes = await fetch('https://icanhazdadjoke.com/', {      // await makes a function wait for a Promise
         headers: {
             'Accept': 'application/json'      
 
@@ -14,7 +14,7 @@ async function generateJoke(){
         }
     });
 
-    const joke = await jokeRes.json();     
+    const joke = await jokeRes.json();  // save     
 
  
     jokeEl.innerHTML = joke.joke;    
